12d8aa8fb265c71bf3b3a049d26c7f177ba3f0e8
AbdulRehman <122511494+AbdulRehmamn@users.noreply.github.com>
2025-07-07 22:18:01 +0500

    Fix malformed regular expression in extractVideoId function

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [downloadLink, setDownloadLink] = useState(null);
 
   const extractVideoId = (url) => {
-    const regExp = /^.(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : url;
   };
